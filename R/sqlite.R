172e8847f02dcfe2fec3b6700b6e6288737a1235
Graham Parsons <graham@grahamrp.com>
2019-10-26 19:54:15 +0100

    Add minimal sqlite implementation

--- R/sqlite.R
+++ R/sqlite.R
@@ -1,26 +1,68 @@
-# Given a query function and a count function, return a funcFilter
-# as required by DT
-sql_filter_factory <- function(con, get_page, get_count, ...) {
+#' Sqlite implementation of a query_fun.
+#'
+#' Simple implementation showing how to implement a \code{query_fun} for a sqlite DB.
+#'
+#' This function can be provided to sql_filter_factory to describe how to
+#' fetch a datatable payload from a sqlite table.
+#'
+#' Use as a template to implement other backends.
+#'
+#' Will be called via \code{sql_filter_factory} with arguments
+#' \code{con}, \code{page}, and \code{...} where \code{...} are extra arguments
+#' passed to \code{sql_filter_factory}.
+#'
+#' @param con DBI database connection
+#' @param params named list provided by a datatable containing sorting, filtering
+#'   and pagination data.
+#' @param tbl string, the table/view in sqlite.
+#' @export
+#' @examples
+#' \dontrun{
+#' sql_filter_factory(con, query_sqlite, tbl = "mtcars")
+#' }
+query_sqlite <- function(con, params, tbl) {
 
-  con <- force(con)
+  recordsTotal = get_sqlite_count(con, tbl)
+  recordsFiltered = recordsTotal  # Not implemented
+  data = get_sqlite_page(con, params, tbl)
+  DT_rows_all = NULL  # Not implemented
+  DT_rows_current = NULL  # Not implemented
 
-  query_fun <- function(params) {
-    get_page(con, params, ...)
-  }
-
-  count_fun <- function() {
-    get_count(con, ...)
-  }
-
-  function(data, params) {
-    n <- count_fun()
-
-    # users may be updating the table too frequently
-    if (cols_out_of_sync(data, params)) return(empty_payload(n, params$draw))
-
-    page <- query_fun(params)
-    page <- escape_data(page, params$escape)
-    format_payload(page, n, params$draw)
-  }
-
+  list(
+    recordsTotal = recordsTotal,
+    recordsFiltered = recordsFiltered,
+    data = data,
+    DT_rows_all = DT_rows_all,
+    DT_rows_current = DT_rows_current
+  )
 }
+
+# Get rows from tbl for the visible datatable page
+get_sqlite_page <- function(con, params, tbl) {
+  # Translate dt query params into a sql query
+  query <- glue::glue_sql("SELECT * FROM {`tbl`} ",
+                    order_by_clause(params),
+                    " LIMIT {params$length} OFFSET {params$start}",
+                    .con = con)
+  message(query)
+  DBI::dbGetQuery(con, query)
+}
+
+# Get the total record count in tbl
+get_sqlite_count <- function(con, tbl) {
+    query <- glue::glue_sql("SELECT COUNT (*) AS n FROM {tbl}", .con = con)
+    result <- DBI::dbGetQuery(con, query)
+    result$n
+}
+
+# Return ORDER BY clause or "", accounting for zero-based col indexing of q
+order_by_clause <- function(params) {
+  order <- params$order
+  if (is.null(order)) return("")
+
+  orderings <- purrr::map_chr(order, ~paste(as.integer(.x$column) + 1, .x$dir))
+  orderings <- paste(orderings, collapse = ", ")
+
+  paste("ORDER BY", orderings)
+}
+
--- inst/app.R
+++ inst/app.R
@@ -2,6 +2,10 @@ library(shiny)
 library(DT)
 library(DBI)
 
+# Setup example database and table
+con <- dbConnect(RSQLite::SQLite(), ":memory:")
+dbWriteTable(con, "mtcars", mtcars)
+
 ui <- fluidPage(
   verbatimTextOutput("debug"),
   DTOutput("tbl")
@@ -13,17 +17,23 @@ server <- function(input, output, session) {
     input$tbl_rows_selected
   })
 
-  con <- dbConnect(RSQLite::SQLite(), ":memory:")
-  dbWriteTable(con, "mtcars", mtcars)
-
+  # Create an initial dataframe. This only needs to contain column names, and
+  # need not have any rows.
   initial_df <- dbGetQuery(con, "SELECT * FROM mtcars LIMIT 0;")
 
+  # Create a funcFilter function describing how to get data for a datatable
+  mtcars_filter <- sql_filter_factory(
+    con,  # Use the connection created above
+    query_sqlite,  # Use the query_sqlite implementation (or provide your own)
+    tbl = "mtcars"  # Optional, additional args are passed into query_sqlite
+  )
 
-  myF <- sql_filter_factory(con, get_page, get_count, tbl = "mtcars")
-
-  output$tbl <- renderDT(initial_df, server = TRUE,
-                         rownames = T, funcFilter = myF)
-
+  output$tbl <- renderDT(
+    initial_df,
+    server = TRUE,  # Must be TRUE to perform processing in R, not in the browser
+    rownames = FALSE,  # Must be FALSE
+    funcFilter = mtcars_filter  # Provide the sqlite function filter
+  )
 }
 
 shinyApp(ui, server)
