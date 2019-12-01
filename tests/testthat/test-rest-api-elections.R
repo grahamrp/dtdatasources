f882fa9184f801851e9fb03c0b499be6f38c1703
Graham Parsons <graham@grahamrp.com>
2019-12-01 19:13:14 +0000

    Update test

--- tests/testthat/test-rest-api-elections.R
+++ tests/testthat/test-rest-api-elections.R
@@ -2,9 +2,9 @@ test_that("make_sort_parameter", {
   params <- list(
     order = list(`0` = list(column = "0", dir = "asc"))
   )
-  col_index <-  c("0" = "col_a", "1" = "col_b")
+  col_index <-  c("0" = "electionType", "1" = "label")
   result <- make_sort_parameter(params, col_index)
-  expected <- "col_a"
+  expected <- "electionType"
   expect_equal(result, expected)
 })
 
@@ -13,13 +13,14 @@ test_that("make_sort_parameter with multiple sorts", {
     order = list(`0` = list(column = "0", dir = "asc"),
                  `1` = list(column = "2", dir = "desc"))
   )
-  col_index <-  c("0" = "col_a", "1" = "col_b")
+  col_index <-  c("0" = "electionType", "1" = "label")
   result <- make_sort_parameter(params, col_index)
   # only first order parameter should be used
-  expected <- "col_a"
+  expected <- "electionType"
   expect_equal(result, expected)
 })
 
+# Conversion of a single param$order term
 test_that("to_sort_param", {
   col_index <- c("0" = "col_a", "1" = "col_b")
   order_term <- list(column = "1", dir = "asc")
